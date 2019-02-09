85dfc689173aa3d1fabf5ea811428bbf75564203
Richard Barsha <barsha.richard@gmail.com>
2019-02-09 16:33:47 +1100

    Renamed project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FolioFrontend
+# Personel Manager
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'folio-frontend'`, () => {
+  it(`should have as title 'personel-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('folio-frontend');
+    expect(app.title).toEqual('personel-manager');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to folio-frontend!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to personel-manager!');
   });
 });
